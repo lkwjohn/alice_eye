a51f73ee61e9d410dfdc35547eda3cb40c3e8e4f
John <lkwjohn@yahoo.com>
2020-01-05 21:27:21 +0800

    change config

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const RECORD_TIME_IN_MIL_SECONDS = 1000;//540000
+const RECORD_TIME_IN_MIL_SECONDS = 540000
 const BASE_PATH = '/mnt/alice_nas'
 const FROM_EMAIL = 'employeetimesheet2014@gmail.com';
 const ADDRESSES = 'lkwjohn@yahoo.com';
